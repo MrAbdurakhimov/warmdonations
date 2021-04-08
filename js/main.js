034a5724d054d73c9f447e9a15a2b10a2a6b4295
Mukhammadyusuf <icoderx@yandex.ru>
2021-04-08 10:31:53 +0500

    Initialized

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const counter = document.querySelectorAll(".counter");
+let speed = 200;
+
+counter.forEach((counter) => {
+  const updateCount = () => {
+    const target = counter.getAttribute("data-target");
+    const count = +counter.innerText;
+    const inc = target / speed;
+    if (count < target) {
+      counter.innerText = count + inc;
+      setTimeout(updateCount, 10);
+    } else {
+      counter.innerText = target;
+    }
+  };
+  updateCount();
+});
