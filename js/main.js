7cd06ed0af0fdb71948c10407c069d0008f12664
Mukhammadyusuf Abdurakhimov <59238478+MrAbdurakhimov@users.noreply.github.com>
2021-04-18 16:36:10 +0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -6,8 +6,9 @@ counter.forEach((counter) => {
     const target = counter.getAttribute("data-target");
     const count = +counter.innerText;
     const inc = target / speed;
+    inc = Math.float(inc);
     if (count < target) {
-      counter.innerText = Math.round(count + inc);
+      counter.innerText = count + inc;
       setTimeout(updateCount, 10);
     } else {
       counter.innerText = target;
